284bda980ffb1cc517f1b6306d417d8b53761231
Sugni <giaco.salvadori@gmail.com>
2019-01-21 11:25:52 +0100

    [DEV] Added downloadable files

--- download_file.php
+++ download_file.php
@@ -3,18 +3,32 @@
 	
 	$fileCode = $_POST['file_code'];
 	$fileList = array(
-					'0wz9ZB6Oax' => 'creamy_battle.zip'
+					'games' => array(
+									'0wz9ZB6Oax' => 'creamy_battle.zip'
+									),
+					'papers' => array(
+									'puYfP2Kg4P' => 'COD_WaW_Black_cats_ENG.pdf'
+									)
 				);
+	
+	$filePath = "";
+	$file = "";
+	
+	foreach($fileList as $dir => $fileName){
+		if($fileName[$fileCode]){
+			$filePath = __DIR__.'/'.$dir.'/'.$fileName[$fileCode];
+			$file = $fileName[$fileCode];
+			break;
+		}
+	}
 	
 	$fileName = $fileList[$fileCode];
 	
-	if(!$fileName){
+	if(!$filePath){
 		echo '<h1>ACCESS DENIED</h1>';
-	} else {
-		$file_path = __DIR__.'/games/'.$fileName;
-		
+	} else {		
 		header("Content-type: application/zip");
-		header("Content-Disposition: attachment; filename=$fileName");
+		header("Content-Disposition: attachment; filename=$file");
 		header("Pragma: no-cache"); 
 		header("Expires: 0"); 
 		readfile("$file_path");
